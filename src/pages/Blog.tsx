7ea955f976777a60d257a869537069b634687a1a
Vitaly Vorobyev <vit.vorobiev@gmail.com>
2026-02-22 20:27:14 +0100

    bun.lock updated

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default function Blog() {
     return (
@@ -11,8 +11,9 @@ export default function Blog() {
             </div>
 
             <div className="space-y-6">
+                Stay tuned!
                 {/* Placeholder for blog posts */}
-                <div className="p-6 rounded-xl border border-border hover:border-foreground/20 transition-colors group">
+                {/* <div className="p-6 rounded-xl border border-border hover:border-foreground/20 transition-colors group">
                     <Link to="/blog/example">
                         <h2 className="text-2xl font-semibold group-hover:underline">Camera Calibration and PnP</h2>
                         <p className="text-muted-foreground mt-2">
@@ -20,7 +21,7 @@ export default function Blog() {
                         </p>
                         <div className="text-sm font-mono text-muted-foreground mt-4">Oct 24, 2026</div>
                     </Link>
-                </div>
+                </div> */}
             </div>
         </div>
     );
